51b52ba932759b16f8f26e23ca43370a42297b9c
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-10-02 18:45:41 +0300

    [task] add sass and start header styling

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -5,7 +5,11 @@ Modal.setAppElement("#model");
 
 const OptionModal = props => {
   return (
-    <Modal isOpen={!!props.selectedOption} contentLabel="Selected Option">
+    <Modal
+      isOpen={!!props.selectedOption}
+      contentLabel="Selected Option"
+      onRequestClose={props.HandleClearSelectedOption}
+    >
       <h3>Selected Option</h3>
       {props.selectedOption && <p>{props.selectedOption}</p>}
       <button onClick={props.HandleClearSelectedOption}>Okay</button>
